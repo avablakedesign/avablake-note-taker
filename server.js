2198b065b93f3c21175c6952de6d6ac712824908
Ava Blake <avablakedesign@protonmail.com>
2022-11-12 10:49:28 -0500

    Route handlers set up for sending HTML files.

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    console.info(req.hostname);
+    res.send("test");
+})
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
